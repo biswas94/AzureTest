bd05d2f2fb844de097d47e45bcfff2b6e3b81e77
biswas94 <bis_sharma2000@yahoo.com>
2015-10-26 14:49:15 +0000

    established mysql connection

--- index.php
+++ index.php
@@ -6,6 +6,20 @@
 </head>
 <body>
     <?php
+        $servername = "eu-cdbr-azure-north-d.cloudapp.net";
+        $username = "b0cae9e0d4c5fc";
+        $password = "fdcc89df";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected successfully";
+    ?>
+    <?php
         for($i = 0; $i<5; $i++){
             echo "<H1> It's ALIVE!!!</H1>";
         }
