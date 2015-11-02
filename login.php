f35d35ce05873d96990f91022cfdd12dfcb5938c
biswas94 <bis_sharma2000@yahoo.com>
2015-11-02 14:21:21 +0000

    login.php added

--- login.php
+++ login.php
@@ -7,11 +7,15 @@
 <body>
 <?php
 $servername = "eu-cdbr-azure-north-d.cloudapp.net";
+echo "<form>";
 echo "<h2>Login</h2>";
 echo "<h3>Username: </h3><input type='text' id='username' />";
-echo "<h3>Password: </h3><input type='password' id='password' />";
+echo "<h3>Password: </h3><input type='password' id='password' /><br>";
 echo "<input type='submit' id='submit_button' value='Login' />";
+echo "</form>";
 setcookie(‘access_level’,'standarduser');
+
+setcookie('name','', 1417608000,'/login.php','http://biswas-azure.azurewebsites.net');
     ?>
 </body>
 </html>
